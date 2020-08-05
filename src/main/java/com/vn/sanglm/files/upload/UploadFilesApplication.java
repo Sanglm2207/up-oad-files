ef619fffdd55afe7208141dbee2a810cbbbcc739
Lại Minh Sáng <Sanglm2207@gmail.com>
2020-08-05 21:23:30 +0700

    first commit

---
+++ src/main/java/com/vn/sanglm/files/upload/UploadFilesApplication.java
@@ -0,0 +1,13 @@
+package com.vn.sanglm.files.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadFilesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/vn/sanglm/files/upload/repository/TutorialRepository.java
@@ -0,0 +1,7 @@
+package com.vn.sanglm.files.upload.repository;
+
+import com.vn.sanglm.files.upload.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+}
